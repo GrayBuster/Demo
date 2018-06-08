338682b01fd226611859c4c07a3fab6695984ef0
gray buster <imkhanh417@gmail.com>
2018-06-08 14:09:04 +0700

    Initial Commit

---
+++ DemoPoppup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoPoppup
+//
+//  Created by gray buster on 6/8/18.
+//  Copyright © 2018 gray buster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
